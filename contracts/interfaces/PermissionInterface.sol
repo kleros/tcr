a095451d27dcc156a08c2c3d3125a4656297ae49
Matheus A <malencar@pm.me>
2019-06-30 20:07:27 -0300

    feat: add t2cr and badge contracts

---
+++ contracts/interfaces/PermissionInterface.sol
@@ -0,0 +1,25 @@
+/**
+ *  @authors: [@mtsalenc]
+ *  @reviewers: []
+ *  @auditors: []
+ *  @bounties: []
+ *  @deployments: []
+ */
+
+
+pragma solidity ^0.5.8;
+
+/**
+ *  @title Permission Interface
+ *  This is a permission interface for arbitrary values. The values can be cast to the required types.
+ */
+interface PermissionInterface{
+    /* External */
+
+    /**
+     *  @dev Return true if the value is allowed.
+     *  @param _value The value we want to check.
+     *  @return allowed True if the value is allowed, false otherwise.
+     */
+    function isPermitted(bytes32 _value) external view returns (bool allowed);
+}
